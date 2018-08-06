4acc7928a71da4018a8177b66defb11e27b004e2
Vicki Hamm <vicki.cello@gmail.com>
2018-08-06 21:12:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,10 +70,8 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(number)
-    sql = "SELECT * FROM students where grade = 10 ORDER BY students.id LIMIT ?;"
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(sql, number)
-    end
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT ?;"
+    DB[:conn].execute(sql, number)
   end
 
   def self.first_student_in_grade_10
