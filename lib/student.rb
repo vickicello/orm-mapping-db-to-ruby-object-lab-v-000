33d276cebf1c807afc1d0e1a60ca426d00a50bcf
Vicki Hamm <vicki.cello@gmail.com>
2018-08-06 20:58:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
-    new_student.name =  row[1]
+    new_student.name = row[1]
     new_student.grade = row[2]
     new_student
   end
